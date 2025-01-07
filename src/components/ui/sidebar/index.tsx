50284688b567b9bf2251e2455ddd67d302fe5da9
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-07 21:36:06 +0000

    Refactoring routes to be underneath userId slug

--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -11,28 +11,28 @@ export function SidebarLayout() {
   const links = [
     {
       label: "Dashboard",
-      href: "/dashboard",
+      href: "/dashboard/1234",
       icon: (
         <House className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
       label: "Automations",
-      href: "/automations",
+      href: "/dashboard/1234/automations",
       icon: (
         <Workflow className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
         label: "Integrations",
-        href: "/integrations",
+        href: "/dashboard/1234/integrations",
         icon: (
           <Blocks className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
         ),
       },
     {
       label: "Settings",
-      href: "#",
+      href: "/dashboard/1234/settings",
       icon: (
         <Settings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
@@ -47,12 +47,6 @@ export function SidebarLayout() {
   ];
   const [open, setOpen] = useState(false);
   return (
-    // <div
-    //   className={cn(
-    //     "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-    //     "h-screen"
-    //   )}
-    // >
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
@@ -81,10 +75,8 @@ export function SidebarLayout() {
             />
           </div>
         </SidebarBody>
-      </Sidebar> );
-      {/* <Dashboard /> */}
-    // </div>
-//    );
+      </Sidebar> 
+  );
 }
 export const Logo = () => {
   return (
@@ -111,31 +103,5 @@ export const LogoIcon = () => {
     >
       <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
     </Link>
-  );
-};
-
-// Dummy dashboard component with content
-const Dashboard = () => {
-  return (
-    <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-      </div>
-    </div>
   );
 };
