fd29c7037ed2f71ad8352d510de881dfaf3fdbf2
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-07 17:48:50 +0000

    Adding protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(
-    ['/dashboard(.*)']
-)
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/settings(.*)',
+    '/integrations(.*)',
+    '/automations(.*)'
+])
 
-export default clerkMiddleware();
+export default clerkMiddleware(async ( auth, req ) => {
+    if(isProtectedRoute(req)) await auth.protect()
+});
 
 export const config = {
   matcher: [
