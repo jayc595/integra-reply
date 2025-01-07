059addd03e842a6425d9829e4e57ae5c59175071
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-07 17:26:25 +0000

    Adding Clerk authentication

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h2>Landing Page</h2>
+        <Button>Log In</Button>
       </main>
     </div>
   );
