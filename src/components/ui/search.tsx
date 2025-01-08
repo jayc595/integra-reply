ebfb2001256a2ff0d1e0d2c83b7fd3e044b0a5df
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-08 20:34:17 +0000

    Enhancing search visibility using Aceternity component

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -1,15 +1,32 @@
-import { SearchIcon } from 'lucide-react'
+"use client";
+
 import React from 'react'
-import { Input } from './input'
+import { PlaceholdersAndVanishInput } from '../placeholders-and-vanish-input';
 
 type Props = {}
 
 const Search = (props: Props) => {
+    const placeholders = [
+        "Search for Integrations...",
+        "Search for Automations...",
+        "Search for Support...",
+        "Search for Settings...",
+      ];
+
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+      };
+      const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submitted");
+      };
+
   return (
-    <div className='hidden lg:flex overflow-hidden gap-x-2 border-2 border-[#3352cc] rounded-full px-4 py-1 items-center flex-1 flex-grow'>
-        <SearchIcon color='#3352cc'/>
-        <Input placeholder='Search Placeholder' className='border-none outline-none ring-0 focus:ring-0 flex-1'/>
-    </div>
+        <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={handleChange}
+            onSubmit={onSubmit}
+      />
   )
 }
 
