01ed195f70ff06f60282b435ebd67be020d287ea
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-08 22:50:13 +0000

    Shortening integration descriptions

--- src/constants/integrations.tsx
+++ src/constants/integrations.tsx
@@ -10,25 +10,25 @@ type INTEGRATION_CARD_TYPE = {
 export const INTEGRATION_CARD: INTEGRATION_CARD_TYPE[] = [
     {
         title: 'Connect Instagram',
-        description: 'Seamlessly connect your Instagram to IntegraReply and automate your DMs and comments! Save time, boost engagement, and respond instantly to your audience with smart, keyword-driven replies. 📩✨',
+        description: 'Automate your DMs and comments! Save time, boost engagement, and respond instantly to your audience with smart, keyword-driven replies. 📩✨',
         icon: <Instagram />,
         type: 'INSTAGRAM'
     },
     {
         title: 'Connect Facebook',
-        description: 'Connect your Facebook to IntegraReply and automate Messenger responses and post replies! Keep your audience engaged 24/7 with smart, instant replies tailored to their needs. 🚀📬',
+        description: 'Automate Messenger responses and post replies! Keep your audience engaged 24/7 with smart, instant replies tailored to their needs. 🚀📬',
         icon: <Facebook />,
         type: 'FACEBOOK'
     },
     {
         title: 'Connect Whatsapp',
-        description: 'Link your WhatsApp to IntegraReply for automated, real-time responses that keep your conversations flowing. Deliver instant replies and personalized interactions, all hassle-free! 💬🤖',
+        description: 'Real-time responses that keep your conversations flowing. Deliver instant replies and personalized interactions, all hassle-free! 💬🤖',
         icon: <MessageCircle/>,
         type: 'WHATSAPP'
     },
     {
         title: 'Connect Email',
-        description: 'Integrate your email with IntegraReply to automate responses and follow-ups effortlessly. Save time, stay organized, and ensure no message goes unanswered. 📧✔️',
+        description: 'Automate responses and follow-ups effortlessly. Save time, stay organized, and ensure no message goes unanswered. 📧✔️',
         icon: <Mail />,
         type: 'EMAIL'
     }
