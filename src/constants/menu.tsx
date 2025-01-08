66585d7a04f9c32104727f8f8fb4bf37c5097f83
jayc595 <jay.crocker@hotmail.co.uk>
2025-01-08 22:37:52 +0000

    Adding descriptions to other links

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -9,21 +9,22 @@ type MENU_TYPE = {
 
 export const MENU: MENU_TYPE[] = [
     {
-        label: "Dashboard",
-        href: "/dashboard/1234",
+      label: "Dashboard",
+      href: "/dashboard/1234",
       icon: (
         <House className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
-        label: "Automations",
-        href: "/dashboard/1234/automations",
+      label: "Automations",
+      description: "Automate routine processes to focus on what matters most.",
+      href: "/dashboard/1234/automations",
       icon: (
         <Workflow className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
-        label: "Integrations",
+      label: "Integrations",
       description: "Connect your accounts to enable automated responses.",
       href: "/dashboard/1234/integrations",
       icon: (
@@ -31,15 +32,16 @@ export const MENU: MENU_TYPE[] = [
       ),
     },
     {
-        label: "Settings",
-        href: "/dashboard/1234/settings",
+      label: "Settings",
+      description: "Customize your preferences and manage your account settings.",
+      href: "/dashboard/1234/settings",
       icon: (
         <Settings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
     },
     {
-        label: "Logout",
-        href: "#",
+      label: "Logout",
+      href: "#",
       icon: (
         <LogOut className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
